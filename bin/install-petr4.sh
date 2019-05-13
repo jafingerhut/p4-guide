e01f45f2193778843dc6b5e326a9c6748c5d2ffc
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-05-13 09:45:58 -0700

    Updates to install-petr4.sh script

--- bin/install-petr4.sh
+++ bin/install-petr4.sh
@@ -14,21 +14,64 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
+# Remember the current directory when the script was started:
+INSTALL_DIR="${PWD}"
+
+warning() {
+    1>&2 echo "This install script has only been tested on Ubuntu 18.04 so far."
+    1>&2 echo "Proceed installing manually at your own risk of significant time"
+    1>&2 echo "spent figuring out how to make it all work, or consider getting"
+    1>&2 echo "VirtualBox and creating an Ubuntu 18.04 virtual machine."
+}
+
+lsb_release >& /dev/null
+if [ $? != 0 ]
+then
+    1>&2 echo "No 'lsb_release' found in your command path."
+    warning
+    exit 1
+fi
+
+distributor_id=`lsb_release -si`
+release=`lsb_release -sr`
+if [ "${distributor_id}" = "Ubuntu" -a "${release}" = "18.04" ]
+then
+    echo "Found distributor '${distributor_id}' release '${release}'.  Continuing with installation."
+else
+    warning
+    1>&2 echo ""
+    1>&2 echo "Here is what command 'lsb_release -a' shows this OS to be:"
+    lsb_release -a
+    exit 1
+fi
+
+echo "------------------------------------------------------------"
+echo "Time and disk space used before installation begins:"
+date
+df -h .
+df -BM .
 
 # Install Ubuntu packages needed for a C compiler, which `opam init`
-# command requires.
+# command requires.  The build-essential package might be more than
+# the minimum required by `opam init`, but probably not a lot more.
 sudo apt-get --yes install curl build-essential
 
 echo "----------------------------------------------------------------------"
 echo "Installing opam:"
+set -x
 curl --location --output /tmp/opam https://github.com/ocaml/opam/releases/download/2.0.4/opam-2.0.4-x86_64-linux
 sudo /bin/cp /tmp/opam /usr/local/bin/opam
 sudo chmod 755 /usr/local/bin/opam
 /bin/rm /tmp/opam
+set +x
 
-echo "You will need to respond to two prompts during the execution of 'opam init'."
+echo "Because I do not know how to invoke the `opam init` command in a way"
+echo "that answers the questions as I have tested, you will need to respond"
+echo "to two prompts during the execution of 'opam init'."
+echo ""
 echo "TBD: Describe the prompts and how one should respond here."
+
+set -x
 opam init
 
 opam env
@@ -37,16 +80,32 @@ eval `opam env`
 # Show version of ocamlc installed, in case it is older than what
 # is required by petr4
 ocamlc -v
+set +x
 
 echo "----------------------------------------------------------------------"
 echo "Installing petr4:"
 
 # According to petr4 README, these packages should be installed.
+set -x
 sudo apt-get --yes install m4 libgmp-dev
-# I create a new directory to run the 'ocam pin add petr4 .' command within,
-# because when I tried running it in my $HOME directory, there was an error
-# message, something about home and $HOME and a directory within $HOME/.opam
-# overlapping each other.
-mkdir install
-cd install
+git clone https://github.com/cornell-netlab/petr4
+cd petr4
 opam pin add petr4 .
+set +x
+
+echo "------------------------------------------------------------"
+echo "Time and disk space used when installation was complete:"
+date
+df -h .
+df -BM .
+
+cd "${INSTALL_DIR}"
+echo "eval `opam env`" > petr4setup.bash
+
+echo ""
+echo "Created file: petr4setup.bash"
+echo ""
+echo "If you use a Bash-like command shell, you may wish to copy the"
+echo "lines of the file petr44setup.bash to your .bashrc or .profile"
+echo "files in your home directory to add the command petr4 to your"
+echo "command path every time you log in or create a new shell."
