089588a41bc10c15adb767a3c45668d9194fbaa8
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-01-07 11:23:09 -0800

    Add README and Python program to break up range matches ... into a short list of prefix value/masks.

---
+++ match-range-using-tcam/README.md
@@ -0,0 +1,216 @@
+It is well known, at least among people who have worked on packet
+classification for ACLs (Access Control Lists), that one can match an
+arbitrary [min, max] range of a W-bit wide field using at most 2W-2
+TCAM entries, for a single field.
+
+There are mentions of this in published research papers, without
+explicitly giving the construction, since approximately the year 2000,
+I am nearly certain that the technique described here was first
+published in the 1990s, but do not have a precise reference (please
+send me one if you know so I can add that here).
+
+The basic idea is to partition the set of values in the range [min,
+max] into one or more disjoint ranges whose union contains exactly the
+same set of values as the original range.
+
+Also, each of these ranges can be represented with a single prefix
+value/mask, where by a prefix value/mask I mean one where the mask is
+exact match on 0 or more consecutive most significant bits of the
+field, and don't-care or wildcard on the remaining least significant
+bits.
+
+A prefix value/mask always matches a set of values in an "aligned
+power of 2 range".  That is, the range will be of the form `[a,
+a+size-1]` where `X` is the number of wildcard bit positions,
+`size=2^X`, and `a` is an integer multiple of `size`.
+
+For a given desired range `[min, max]`, and some prefix value/mask
+that matches exactly the field values in range `[a, b]` that we are
+considering, there are three possibilities:
+
++ Case 1: `[a, b]` lies completely within `[min, max]`, i.e. `min <= a
+  <= b <= max`.  `[a, b]` is OK to use as part of the implementation.
+
++ Case 2: `[a, b]` lies completely outside of the range `[min, max]`.
+  That is, either `b < min`, or `max < a`.  We should discard such a
+  range `[a, b]` entirely.
+
++ Case 3: `[a, b]` contains some value within `[min, max]`, but also
+  some values outside of `[min, max]`.  This is if it is neither of
+  the cases above.  We should split the prefix range into two prefix
+  ranges, where one contains the first half of the values in `[a, b]`,
+  and the other contains the last half.  Then consider each of those
+  two ranges independently, to determine which of these 3 cases is
+  true for them.
+
+Thus one algorithm is to start with the prefix value/mask that is
+wildcard in all W bit positions, which matches the range `[0, 2^W-1]`.
+Determine which of the 3 cases above is true for it.  If it is case 1,
+we are done with that one prefix value/mask.  If it is case 3, we
+recurse on the two new prefix value/mask ranges.
+
+Example 1:
+
+A field with width W=4 bits, for which we want to match the range with
+`min=1` and `max=6` decimal.  The `min` and `max` values written in
+binary are:
+
+```
+min   0001   (1 decimal)
+max   0101   (5 decimal)
+      ^  ^ bit position 0
+     bit
+     position
+      3
+```
+
+In the examples, we will represent a value/mask as a string that
+contains x for wildcard bit positions, or a 0 or 1 for an exact match
+bit position.
+
+```
+Reminder of input range values:
+0001   min = 1 decimal
+0101   max = 5 decimal
+
+The initial prefix to consider is:
+
+xxxx   prefix is range [0, 15].  Case 3.  Split into ranges (0) and (1) below.
+
+(0)
+0xxx   prefix is range [0, 7].  Case 3.  Split into ranges (00) and (01) below.
+
+(00)
+00xx   prefix is range [0, 3].  Case 3.  Split into ranges (000) and (001) below.
+
+(000)
+000x   prefix is range [0, 1].  Case 3.  Split into ranges (0000) and (0001) below.
+
+(0000)
+0000   prefix is range [0, 0].  Case 2.  Discard.
+
+(0001)
+0001   prefix is range [1, 1].  Case 1.  Keep.
+
+(001)
+001x   prefix is range [2, 3].  Case 1.  Keep.
+tbd
+
+(01)
+01xx   prefix is range [4, 7].  Case 3.  Split into ranges (010) and (010) below.
+
+(010)
+010x   prefix is range [4, 5].  Case 1.  Keep.
+
+(011)
+011x   prefix is range [6, 7].  Case 2.  Discard.
+
+(1)
+1xxx   prefix is range [8, 15].  Case 2.  Discard.
+```
+
+The "case 1. keep" cases above are for the following ranges, which is
+the result of the range expansion of [1, 5] for W=4 bits:
+
+```
+0001   prefix is range [1, 1].  Case 1.  Keep.
+001x   prefix is range [2, 3].  Case 1.  Keep.
+010x   prefix is range [4, 5].  Case 1.  Keep.
+```
+
+The included Python program `range-to-tcam-entries.py` does not use
+exactly the algorithm above, but produces the same result.
+
+```
+$ ./range-to-tcam-entries.py --bit-width 4 --min 1 --max 5
+debug=False
+W=4
+min=1
+max=5
+idx value_hex mask_hex range_min_decimal range_max_decimal
+ 0          1          f          1          1
+ 1          2          e          2          3
+ 2          4          e          4          5
+```
+
+In general, the range `[1, 2^W-2]` causes the largest number of
+value/masks to be produced, `2W - 2` of them.  Here is sample output
+for `W=16`:
+
+```
+$ ./range-to-tcam-entries.py --bit-width 16 --min 1 --max 65534
+debug=False
+W=16
+min=1
+max=65534
+idx value_hex mask_hex range_min_decimal range_max_decimal
+ 0          1       ffff          1          1
+ 1          2       fffe          2          3
+ 2          4       fffc          4          7
+ 3          8       fff8          8         15
+ 4         10       fff0         16         31
+ 5         20       ffe0         32         63
+ 6         40       ffc0         64        127
+ 7         80       ff80        128        255
+ 8        100       ff00        256        511
+ 9        200       fe00        512       1023
+10        400       fc00       1024       2047
+11        800       f800       2048       4095
+12       1000       f000       4096       8191
+13       2000       e000       8192      16383
+14       4000       c000      16384      32767
+15       8000       c000      32768      49151
+16       c000       e000      49152      57343
+17       e000       f000      57344      61439
+18       f000       f800      61440      63487
+19       f800       fc00      63488      64511
+20       fc00       fe00      64512      65023
+21       fe00       ff00      65024      65279
+22       ff00       ff80      65280      65407
+23       ff80       ffc0      65408      65471
+24       ffc0       ffe0      65472      65503
+25       ffe0       fff0      65504      65519
+26       fff0       fff8      65520      65527
+27       fff8       fffc      65528      65531
+28       fffc       fffe      65532      65533
+29       fffe       ffff      65534      65534
+```
+
+The Python program also provides a `--test` option, which causes the
+program to exhaustively try _all_ ranges with the given width in bits,
+approximately `2^(2*W)` of them, so testing with `W > 10` can require
+significant patience.  For each of those ranges, it will call the
+Python function `range_to_tcam_entries` to compute the list prefix
+value/masks for the range, then call `check_tcam_entries` to verify
+that they are prefix masks, that each covers a disjoint range, and
+that together they cover the entire input range.  It will raise an
+exception if any incorrect list of prefix value/masks is found.
+
+I have run the exhaustive test for all bit widths from 1 up to 11 and
+it found no errors, so I have very high confidence that the code for
+calculating lists of prefix value/masks is correct.
+
+Note: I chose Python because its integer arithmetic and bit-wise
+operations work on arbitrary size integers without any special effort
+on my part.  Porting this to another programming language would
+require all of those operations to work up to the largest supported
+bit width of values you want to support for the ranges.
+
+
+Extensions:
+
+If one wanted to then match on two independent fields simultaneously
+using [min, max] ranges, e.g. a layer 4 16-bit source port and 16-bit
+destination port, as used in UDP and TCP packets, you can perform the
+"range to TCAM-entry expansion" described here independently for each
+of the two fields, yielding a set S1 of TCAM value/masks for field F1,
+and a set S2 for field F2.  Then perform the cross product of the sets
+S1 and S2.
+
+
+Note that the set of TCAM entries produced are such that at most one
+of them can match any individual field.  This is not a critical
+property when populating the entries in a TCAM, where it is acceptable
+for many entries to match, and the highest priority will "win", but
+there may be contexts in which this property is important, e.g. some
+algorithmic TCAM approaches.
---
+++ match-range-using-tcam/range-to-tcam-entries.py
@@ -0,0 +1,257 @@
+#! /usr/bin/env python3
+
+import os, sys
+import re
+
+######################################################################
+# Parsing optional command line arguments
+######################################################################
+
+import argparse
+
+debug = False
+
+parser = argparse.ArgumentParser(description="""
+Print a small number of TCAM entries that together match any value
+in the range [min, max], where the value to match is bit-width bits wide.""")
+parser.add_argument('--debug', dest='debug', action='store_true',
+                    help="""Enable extra debug messages.""")
+parser.add_argument('--test', dest='test', action='store_true',
+                    help="""Perform exhaustive test of _all_ ranges with the given width in bits.  Warnings: this is reasonably fast for W <= 10, but the number of test cases grows roughly as 2^(2*W)""")
+parser.add_argument('--bit-width', dest='W', type=int, required=True,
+                    help="""The width in bits of the field to match on.""")
+parser.add_argument('--min', dest='min', type=int, required=True,
+                    help="""The minimum value of the range to match.""")
+parser.add_argument('--max', dest='max', type=int, required=True,
+                    help="""The maximum value of the range to match.""")
+args = parser.parse_known_args()[0]
+
+debug = args.debug
+print("debug=%s" % (debug))
+print('W=%d' % (args.W))
+print('min=%d' % (args.min))
+print('max=%d' % (args.max))
+
+all_prefix_masks = {}
+
+def calculate_all_prefix_masks_dict(W):
+    all_prefix_masks_dict = {}
+    W_mask = (1 << W) - 1
+    mask = W_mask
+    while mask != 0:
+        all_prefix_masks_dict[mask] = True
+        mask = (mask << 1) & W_mask
+    all_prefix_masks_dict[0] = True
+    return all_prefix_masks_dict
+
+
+def is_prefix_mask(W, mask):
+    if W not in all_prefix_masks:
+        all_prefix_masks[W] = calculate_all_prefix_masks_dict(W)
+    return mask in all_prefix_masks[W]
+
+
+def is_value_mask(W, entry):
+    assert type(W) is int
+    assert type(entry) is dict
+    assert "value" in entry
+    assert "mask" in entry
+    assert type(entry["value"]) is int
+    assert type(entry["mask"]) is int
+    max_field_val = (1 << W) - 1
+    value = entry["value"]
+    mask = entry["mask"]
+    assert value >= 0 and value <= max_field_val
+    assert mask >= 0 and mask <= max_field_val
+    if (value & mask) != value:
+        print("W=%d value=%d mask=%d" % (W, value, mask))
+    assert (value & mask) == value
+    return True
+
+
+def is_prefix_value_mask(W, entry):
+    assert is_value_mask(W, entry)
+    assert is_prefix_mask(W, entry["mask"])
+    return True
+
+
+def prefix_mask_range(W, entry):
+    assert is_prefix_value_mask(W, entry)
+    W_mask = (1 << W) - 1
+    return {"min": entry["value"],
+            "max": entry["value"] + (W_mask ^ entry["mask"])}
+
+
+def part_tcam_entries(W, extreme_val, bitpos, side):
+    W_mask = (1 << W) - 1
+    tcam_entries = []
+    if debug:
+        print("W=%d extreme_val=%d bitpos=%d side=%s"
+              "" % (W, extreme_val, bitpos, side))
+    while bitpos >= 0:
+#        if debug:
+#            print("part_tcam_entries step #1: bitpos=%d"
+#                  "" % (bitpos))
+        mask = (1 << bitpos) - 1
+        if side == 'left':
+            check_val = 0
+        else:
+            check_val = mask
+        if (extreme_val & mask) == check_val:
+            entry = {"value": extreme_val ^ check_val, "mask": W_mask ^ mask}
+            if debug:
+                debug_range = prefix_mask_range(W, entry)
+                print("bitpos=%d value=%d mask=%d min=%d max=%d"
+                      "" % (bitpos, entry["value"], entry["mask"],
+                            debug_range["min"], debug_range["max"]))
+            tcam_entries.append(entry)
+            break
+        bitpos = bitpos - 1
+        bitpos_mask = 1 << bitpos
+        if side == 'left':
+            check_val = 0
+        else:
+            check_val = bitpos_mask
+        if (extreme_val & bitpos_mask) == check_val:
+            lsb_mask = bitpos_mask - 1
+            tmp_mask = W_mask ^ lsb_mask
+            entry = {"value": (extreme_val ^ bitpos_mask) & tmp_mask,
+                     "mask": tmp_mask}
+            if debug:
+#                print("part_tcam_entries case #2: bitpos=%d value=%d mask=%d"
+#                      "" % (bitpos, entry["value"], entry["mask"]))
+                debug_range = prefix_mask_range(W, entry)
+                print("bitpos=%d value=%d mask=%d min=%d max=%d"
+                      "" % (bitpos, entry["value"], entry["mask"],
+                            debug_range["min"], debug_range["max"]))
+            tcam_entries.append(entry)
+    return tcam_entries
+
+
+def range_to_tcam_entries(W, min_val, max_val):
+    if type(W) is not int:
+        msg = ("W must be int but got %s" % (type(W)))
+        raise TypeError(msg)
+    if type(min_val) is not int:
+        msg = ("min_val must be int but got %s" % (type(min_val)))
+        raise TypeError(msg)
+    if type(max_val) is not int:
+        msg = ("max_val must be int but got %s" % (type(max_val)))
+        raise TypeError(msg)
+
+    max_field_val = (1 << W) - 1
+
+    #print('max_field_val=%d' % (max_field_val))
+    if min_val < 0 or min_val > max_field_val:
+        msg = ("Error: min=%d outside of range of W-bit value [0, %d]"
+               "" % (min_val, max_field_val))
+        raise ValueError(msg)
+    
+    if max_val < 0 or max_val > max_field_val:
+        msg = ("Error: max=%d outside of range of W-bit value [0, %d]"
+               "" % (max_val, max_field_val))
+        raise ValueError(msg)
+
+    if min_val > max_val:
+        msg = ("Error: min=%d is larger than max=%d"
+               "" % (min_val, max_val))
+        raise ValueError(msg)
+
+    tcam_entries = []
+
+    if min_val == max_val:
+        entry = {"value": min_val, "mask": max_field_val}
+        tcam_entries.append(entry)
+        return tcam_entries
+
+    # else the min and max values are different, so there is some bit
+    # position where they differ.  Find the most significant bit
+    # position where they differ
+    bitpos = W - 1
+    bitpos_mask = 1 << bitpos
+    while (min_val & bitpos_mask) == (max_val & bitpos_mask):
+        bitpos = bitpos - 1
+        bitpos_mask = bitpos_mask >> 1
+
+    # Check for special case of a single TCAM entry covering the
+    # entire range
+    mask = (1 << (bitpos + 1)) - 1
+    if ((min_val & mask) == 0) and ((max_val & mask) == mask):
+        entry = {"value": min_val, "mask": max_field_val ^ mask}
+        tcam_entries.append(entry)
+        return tcam_entries
+
+    # Otherwise, it requires at least 2 TCAM entries to cover the
+    # range: one for the "left part" with most significant differing
+    # bit equal to 0, and one for the "right part" with most
+    # significant differing bit equal to 1.
+    left_part_tcam_entries = part_tcam_entries(W, min_val, bitpos, "left")
+    right_part_tcam_entries = part_tcam_entries(W, max_val, bitpos, "right")
+    #right_part_tcam_entries = []
+    
+    tcam_entries = left_part_tcam_entries + right_part_tcam_entries
+    return tcam_entries
+
+
+def sorted_tcam_entries(W, entries):
+    entries_with_ranges = []
+    for entry in entries:
+        range = prefix_mask_range(W, entry)
+        entry_with_range = {"value": entry["value"],
+                            "mask": entry["mask"],
+                            "range": range}
+        entries_with_ranges.append(entry_with_range)
+    return sorted(entries_with_ranges, key=lambda x: x["range"]["min"])
+    
+
+def debug_print_tcam_entries(W, tcam_entries):
+    n = len(tcam_entries)
+    entries_with_ranges = sorted_tcam_entries(W, entries)
+    print("idx value_hex mask_hex range_min_decimal range_max_decimal")
+    for i in range(n):
+        print("%2d %10x %10x %10d %10d"
+              "" % (i,
+                    entries_with_ranges[i]["value"],
+                    entries_with_ranges[i]["mask"],
+                    entries_with_ranges[i]["range"]["min"],
+                    entries_with_ranges[i]["range"]["max"]))
+
+
+def check_tcam_entries(W, min_val, max_val, entries):
+    entries_with_ranges = sorted_tcam_entries(W, entries)
+    n = len(entries_with_ranges)
+    if entries_with_ranges[0]["range"]["min"] != min_val:
+        msg = ("min value of first range %d != %d = min_val"
+               "" % (entries_with_ranges[0]["range"]["min"], min_val))
+        raise ValueError(msg)
+    if entries_with_ranges[n-1]["range"]["max"] != max_val:
+        msg = ("min value of first range %d != %d = max_val"
+               "" % (entries_with_ranges[n-1]["range"]["max"], max_val))
+        raise ValueError(msg)
+    for i in range(1, n):
+        if entries_with_ranges[i]["range"]["min"] != (entries_with_ranges[i-1]["range"]["max"] + 1):
+            msg = ("min value of range %d is %d but expected one more than max of range %d, which is %d"
+                   "" % (i,
+                         entries_with_ranges[i]["range"]["min"],
+                         i-1,
+                         entries_with_ranges[i-1]["range"]["max"]))
+            raise ValueError(msg)
+    return True
+
+
+#range_to_tcam_entries("5", 1, 4)
+entries = range_to_tcam_entries(args.W, args.min, args.max)
+
+debug_print_tcam_entries(args.W, entries)
+check_tcam_entries(args.W, args.min, args.max, entries)
+
+# Exhaustive test of all ranges that fit in width W bits
+if args.test:
+    n = 0
+    for min_val in range(1 << args.W):
+        for max_val in range(min_val, 1 << args.W):
+            entries = range_to_tcam_entries(args.W, min_val, max_val)
+            check_tcam_entries(args.W, min_val, max_val, entries)
+            n += 1
+    print("Successfully checked %d ranges with width W=%d bits"
+          "" % (n, args.W))
