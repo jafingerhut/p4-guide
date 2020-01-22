b7fbff5d08837e7a924024b22f1794c2c175af72
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-01-21 22:59:10 -0800

    Add small helper test script to clone other repos I use ... for testing the basic functionality of an open source P4 development tool installation.

---
+++ bin/clone-other-test-repos.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# Get copies of other repositories that I use to test an open source
+# P4 development tool installation.
+
+git clone https://github.com/p4pktgen/p4pktgen
+
+# Basic testing of p4pktgen functionality:
+
+# cd p4pktgen
+# ./tools/install.sh
+# ./tools/pytest.sh
+
+git clone https://github.com/p4lang/tutorials
+
+# Basic testing of functionality for tutorials repo:
+
+# cd tutorials/exercises/basic
+# cp solutions/basic.p4 .
+# make run
+
+# at mininet prompt:
+# h1 ping h2
+# Ctrl-C after a few seconds
+# h4 ping h3
+# Ctrl-C after a few seconds
+# Ctrl-D to exit mininet
+
+
+# fairly extensive testing of p4c functionality, and some of
+# behavioral-model.
+
+# cd $HOME/p4c/build
+# make check
