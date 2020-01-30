c166a097ef3507618c6eac0af41e7561d4e6ad81
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-01-30 14:48:13 -0800

    Add notes to scripts that need not-installed p4c executable

--- bin/p4c-dump-many-passes.sh
+++ bin/p4c-dump-many-passes.sh
@@ -3,6 +3,16 @@
 if [ $# -ne 1 ]
 then
     1>&2 echo "usage: `basename $0` <p4_source_filename>"
+    1>&2 echo ""
+    1>&2 echo "Note: You should set up your command path so that an appropriate"
+    1>&2 echo "version of the 'p4c' command is first in your path."
+    1>&2 echo ""
+    1>&2 echo "Note: Some of the command line options to the 'p4c' command"
+    1>&2 echo "used by this script give an error if you are running a 'p4c'"
+    1>&2 echo "executable installed in a system-wide location like"
+    1>&2 echo "/usr/local/bin.  They should work if you are using an"
+    1>&2 echo "a 'p4c' executable that you compiled from source code and is"
+    1>&2 echo "in a directory like $HOME/p4c/build/p4c"
     exit 1
 fi
 
