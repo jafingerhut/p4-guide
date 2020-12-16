84034aa89b9a279da57383e4cd87f8cf456e39df
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-12-16 05:40:33 -0800

    Fixes for test script

--- bin/test-cffi-issue.sh
+++ bin/test-cffi-issue.sh
@@ -6,4 +6,11 @@ lsb_release -a
 find /usr | grep cffi
 large-pkgs.py | grep cffi
 git clone https://github.com/p4lang/behavioral-model
-./behavioral-model/travis/install-nnpy.sh
+python3 -V
+pip3 -V
+pip3 list
+sudo apt get python3-pip
+python3 -V
+pip3 -V
+pip3 list
+bash ./behavioral-model/travis/install-nnpy.sh
