bc7140bd06526ca7e687ac27625137ec84798727
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-12-16 05:38:26 -0800

    Fix file name

--- bin/test-cffi-issue.sh
+++ bin/test-cffi-issue.sh
@@ -6,4 +6,4 @@ lsb_release -a
 find /usr | grep cffi
 large-pkgs.py | grep cffi
 git clone https://github.com/p4lang/behavioral-model
-./behavioral-model/travis/install_nnpy.sh
+./behavioral-model/travis/install-nnpy.sh
