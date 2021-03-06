5061fa60dc6acbb5a8a12b1d74afa1228e3125c7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2021-03-06 10:35:52 -0800

    Add a little more debug output to install-p4dev-v4.sh script

--- bin/install-p4dev-v4.sh
+++ bin/install-p4dev-v4.sh
@@ -490,6 +490,8 @@ sudo make install
 # I believe the following 2 commands, adapted from similar commands in
 # src/python/grpcio/README.rst, should install the Python3 module
 # grpc.
+find /usr/lib /usr/local $HOME/.local | sort > $HOME/usr-local-2b-before-grpc-pip3.txt
+pip3 list | tee $HOME/pip3-list-2b-before-grpc-pip3.txt
 sudo pip3 install -rrequirements.txt
 GRPC_PYTHON_BUILD_WITH_CYTHON=1 sudo pip3 install .
 sudo ldconfig
