adb76a2c14916c2f1c071b4fbde6b6e9188674a0
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2021-04-05 06:41:29 -0700

    Add short PTF test for demo1 program, and a script runptf.sh that can run it

---
+++ demo1/ptf/demo1.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Andy Fingerhut, andy.fingerhut@gmail.com
+
+import logging
+import ptf
+import os
+from ptf import config
+import ptf.testutils as tu
+
+from google.rpc import code_pb2
+
+import base_test as bt
+
+######################################################################
+# Configure logging
+######################################################################
+
+# Note: I am not an expert at configuring the Python logging library.
+# Recommendations welcome on improvements here.
+
+# The effect achieved by the code below seems to be that many DEBUG
+# and higher priority logging messages go to the console, and also to
+# a file named 'ptf.log'.  Some of the messages written to the
+# 'ptf.log' file do not go to the console, and appear to be created
+# from within the ptf library.
+
+logger = logging.getLogger(None)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# Examples of some kinds of calls that can be made to generate
+# logging messages.
+#logger.debug("10 logger.debug message")
+#logger.info("20 logger.info message")
+#logger.warn("30 logger.warn message")
+#logger.error("40 logger.error message")
+#logging.debug("10 logging.debug message")
+#logging.info("20 logging.info message")
+#logging.warn("30 logging.warn message")
+#logging.error("40 logging.error message")
+
+
+class Demo1Test(bt.P4RuntimeTest):
+    def setUp(self):
+        bt.P4RuntimeTest.setUp(self)
+        # This setUp method will be executed once for each test case.
+        # It may be a little bit wasteful in time to load the compiled
+        # P4 program for each test, but for only a few tests it is
+        # still quick.  Suggestions welcome on good ways to load the
+        # compiled P4 program only once, yet still allow someone to
+        # select a subset of the test cases to be run from the `ptf`
+        # command line.
+        success = bt.P4RuntimeTest.updateConfig(self)
+        assert success
+
+    #############################################################
+    # Define a few small helper functions that help construct
+    # parameters for the table_add() method.
+    #############################################################
+
+    def key_ipv4_da_lpm(self, ipv4_addr_string, prefix_len):
+        return ('ipv4_da_lpm',
+                [self.Lpm('hdr.ipv4.dstAddr',
+                          bt.ipv4_to_binary(ipv4_addr_string), prefix_len)])
+
+    def act_set_l2ptr(self, l2ptr_int_val):
+        return ('set_l2ptr', [('l2ptr', bt.stringify(l2ptr_int_val))])
+
+    def key_mac_da(self, l2ptr_int_val):
+        return ('mac_da', [self.Exact('meta.fwd_metadata.l2ptr',
+                           bt.stringify(l2ptr_int_val))])
+
+    def act_set_bd_dmac_intf(self, bd_int_val, dmac_string, intf_int_val):
+        return ('set_bd_dmac_intf',
+                [('bd', bt.stringify(bd_int_val)),
+                 ('dmac', bt.mac_to_binary(dmac_string)),
+                 ('intf', bt.stringify(intf_int_val))])
+
+    def key_send_frame(self, bd_int_val):
+        return ('send_frame', [self.Exact('meta.fwd_metadata.out_bd',
+                               bt.stringify(bd_int_val))])
+
+    def act_rewrite_mac(self, smac_string):
+        return ('rewrite_mac', [('smac', bt.mac_to_binary(smac_string))])
+
+
+class FwdTest(Demo1Test):
+    @bt.autocleanup
+    def runTest(self):
+        in_dmac = 'ee:30:ca:9d:1e:00'
+        in_smac = 'ee:cd:00:7e:70:00'
+        ip_dst_addr = '10.1.0.1'
+        ig_port = 1
+
+        eg_port = 2
+        l2ptr = 58
+        bd = 9
+        out_dmac = '02:13:57:ab:cd:ef'
+        out_smac = '00:11:22:33:44:55'
+
+        # Before adding any table entries, the default behavior for
+        # sending in an IPv4 packet is to drop it.
+        pkt = tu.simple_tcp_packet(eth_src=in_smac, eth_dst=in_dmac,
+                                   ip_dst=ip_dst_addr, ip_ttl=64)
+        tu.send_packet(self, ig_port, pkt)
+        tu.verify_no_other_packets(self)
+        
+        # Add a set of table entries that the packet should match, and
+        # be forwarded out with the desired dest and source MAC
+        # addresses.
+        self.table_add(self.key_ipv4_da_lpm(ip_dst_addr, 32),
+                       self.act_set_l2ptr(l2ptr))
+        self.table_add(self.key_mac_da(l2ptr),
+                       self.act_set_bd_dmac_intf(bd, out_dmac, eg_port))
+        self.table_add(self.key_send_frame(bd), self.act_rewrite_mac(out_smac))
+
+        # Check that the entry is hit, expected source and dest MAC
+        # have been written into output packet, TTL has been
+        # decremented, and that no other packets are received.
+        exp_pkt = tu.simple_tcp_packet(eth_src=out_smac, eth_dst=out_dmac,
+                                       ip_dst=ip_dst_addr, ip_ttl=63)
+        tu.send_packet(self, ig_port, pkt)
+        tu.verify_packets(self, exp_pkt, [eg_port])
+
+
+class DupEntryTest(Demo1Test):
+    @bt.autocleanup
+    def runTest(self):
+        ip_dst_addr = '10.0.0.1'
+        l2ptr = 58
+
+        def add_entry_once():
+            self.table_add(self.key_ipv4_da_lpm(ip_dst_addr, 32),
+                           self.act_set_l2ptr(l2ptr))
+
+        add_entry_once()
+        with self.assertP4RuntimeError():
+            add_entry_once()
---
+++ demo1/runptf.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+T="`realpath ../testlib`"
+if [ x"${PYTHONPATH}" == "x" ]
+then
+    P="${T}"
+else
+    P="${T}:${PYTHONPATH}"
+fi
+
+#echo "P is: $P"
+
+# Only show a list of tests
+#ptf --pypath "$P" --test-dir ptf --list
+#exit 0
+
+# Note that the mapping between switch port number and Linux interface
+# names is best to make it correspond with those given when starting
+# the simple_switch_grpc process.  The `ptf` process has no other way
+# of getting this mapping other than by telling it on its command
+# line.
+
+ptf \
+    --pypath "$P" \
+    -i 0@veth1 \
+    -i 1@veth3 \
+    -i 2@veth5 \
+    -i 3@veth7 \
+    -i 4@veth9 \
+    -i 5@veth11 \
+    -i 6@veth13 \
+    -i 7@veth15 \
+    --test-params="grpcaddr='localhost:9559';p4info='demo1.p4_16.p4rt.txt';config='demo1.p4_16.json'" \
+    --test-dir ptf
