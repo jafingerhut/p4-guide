3baa38ea1f1987339a952eaf803f0c60d1aced4f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2021-11-14 01:28:40 -0500

    Add first version of install-p4dev-v5.sh script It uses Radostin Stoyanov's pre-compiled Debian packages for p4c, behavioral-model, and PI.

---
+++ bin/install-p4dev-v5.sh
@@ -0,0 +1,321 @@
+#! /bin/bash
+
+# Copyright 2020-present Intel Corporation
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# This script differs from install-p4dev-v4.sh as follows:
+
+# It install Ubuntu pre-compiled packages created by scripts and
+# published by Radostin Stoyanov, with scripts for creating them here:
+
+# https://github.com/p4lang/p4pi/tree/master/packages
+
+# Remember the current directory when the script was started:
+INSTALL_DIR="${PWD}"
+
+THIS_SCRIPT_FILE_MAYBE_RELATIVE="$0"
+THIS_SCRIPT_DIR_MAYBE_RELATIVE="${THIS_SCRIPT_FILE_MAYBE_RELATIVE%/*}"
+THIS_SCRIPT_DIR_ABSOLUTE=`readlink -f "${THIS_SCRIPT_DIR_MAYBE_RELATIVE}"`
+
+ubuntu_version_warning() {
+    1>&2 echo "This software has only been tested on Ubuntu 20.04"
+    1>&2 echo ""
+    1>&2 echo "Proceed installing manually at your own risk of"
+    1>&2 echo "significant time spent figuring out how to make it all"
+    1>&2 echo "work, or consider getting VirtualBox and creating an"
+    1>&2 echo "Ubuntu virtual machine with one of the tested versions."
+}
+
+abort_script=0
+
+lsb_release >& /dev/null
+if [ $? != 0 ]
+then
+    1>&2 echo "No 'lsb_release' found in your command path."
+    ubuntu_version_warning
+    exit 1
+fi
+
+distributor_id=`lsb_release -si`
+ubuntu_release=`lsb_release -s -r`
+if [ "${distributor_id}" = "Ubuntu" -a \( "${ubuntu_release}" = "20.04" \) ]
+then
+    echo "Found distributor '${distributor_id}' release '${ubuntu_release}'.  Continuing with installation."
+else
+    ubuntu_version_warning
+    1>&2 echo ""
+    1>&2 echo "Here is what command 'lsb_release -a' shows this OS to be:"
+    lsb_release -a
+    exit 1
+fi
+
+# Minimum required system memory is 2 GBytes, minus a few MBytes
+# because from experiments I have run on several different Ubuntu
+# Linux VMs, when you configure them with 2 Gbytes of RAM, the first
+# line of /proc/meminfo shows a little less than that available, I
+# believe because some memory occupied by the kernel is not shown.
+
+min_mem_MBytes=`expr 2 \* \( 1024 - 64 \)`
+memtotal_KBytes=`head -n 1 /proc/meminfo | awk '{print $2;}'`
+memtotal_MBytes=`expr ${memtotal_KBytes} / 1024`
+
+if [ "${memtotal_MBytes}" -lt "${min_mem_MBytes}" ]
+then
+    memtotal_comment="too low"
+    abort_script=1
+else
+    memtotal_comment="enough"
+fi
+
+echo "Minimum recommended memory to run this script: ${min_mem_MBytes} MBytes"
+echo "Memory on this system from /proc/meminfo:      ${memtotal_MBytes} MBytes -> $memtotal_comment"
+
+min_free_disk_MBytes=`expr 12 \* 1024`
+free_disk_MBytes=`df --output=avail --block-size=1M . | tail -n 1`
+
+if [ "${free_disk_MBytes}" -lt "${min_free_disk_MBytes}" ]
+then
+    free_disk_comment="too low"
+    abort_script=1
+else
+    free_disk_comment="enough"
+fi
+
+echo "Minimum free disk space to run this script:    ${min_free_disk_MBytes} MBytes"
+echo "Free disk space on this system from df output: ${free_disk_MBytes} MBytes -> $free_disk_comment"
+
+if [ "${abort_script}" == 1 ]
+then
+    echo ""
+    echo "Aborting script because system has too little RAM or free disk space"
+    exit 1
+fi
+
+PATCH_DIR2="${THIS_SCRIPT_DIR_ABSOLUTE}/patches"
+
+for dir in "${PATCH_DIR2}"
+do
+    if [ -d "${dir}" ]
+    then
+	echo "Found directory containing patches: ${dir}"
+    else
+	echo "NO directory containing patches: ${dir}"
+	abort_script=1
+    fi
+done
+
+if [ "${abort_script}" == 1 ]
+then
+    echo ""
+    echo "Aborting script because an expected directory does not exist (see above)."
+    echo ""
+    echo "This script is not designed to work if you only copy the"
+    echo "script file to a system.  You should run it in the context"
+    echo "of a cloned copy of the repository: https://github/jafingerhut/p4-guide"
+    exit 1
+fi
+
+echo "Passed all sanity checks"
+
+set -e
+set -x
+
+# The maximum number of gcc/g++ jobs to run in parallel.  1 is the
+# safest number that enables compiling p4c even on machines with only
+# 2 GB of RAM, and even on machines with significantly more RAM, it
+# does not speed things up a lot to run multiple jobs in parallel.
+MAX_PARALLEL_JOBS=1
+
+set +x
+echo "This script builds and installs the P4_16 (and also P4_14)"
+echo "compiler, and the behavioral-model software packet forwarding"
+echo "program, that can behave as just about any legal P4 program."
+echo ""
+echo "It is regularly tested on freshly installed Ubuntu 20.04 systems,"
+echo "with all Ubuntu software updates as of the date of"
+echo "testing.  See this directory for log files recording the last"
+echo "date this script was tested on its supported operating systems:"
+echo ""
+echo "    https://github.com/jafingerhut/p4-guide/tree/master/bin/output"
+echo ""
+echo "The files installed by this script consume about 12 GB of disk space."
+echo ""
+echo "On a 2015 MacBook Pro with a decent speed Internet connection"
+echo "and an SSD drive, running Ubuntu Linux in a VirtualBox VM, it"
+echo "took about 90 to 110 minutes."
+echo ""
+echo "Versions of software that will be installed by this script:"
+echo ""
+echo "+ protobuf: github.com/google/protobuf v3.6.1"
+echo "+ gRPC: github.com/google/grpc.git v1.17.2, with patches for Ubuntu 20.04"
+echo "+ PI: github.com/p4lang/PI latest version"
+echo "+ behavioral-model: github.com/p4lang/behavioral-model latest version"
+echo "  which, as of 2021-Jun-28, also installs these things:"
+echo "  + thrift version 0.11.0"
+echo "  + nanomsg version 1.0.0"
+echo "  + nnpy git checkout c7e718a5173447c85182dc45f99e2abcf9cd4065 (latest as of 2015-Apr-22"
+echo "+ p4c: github.com/p4lang/p4c latest version"
+echo "+ ptf: github.com/p4lang/ptf latest version"
+echo "+ Mininet: github.com/mininet/mininet latest version"
+echo "+ Python packages: grpcio 1.17.1, protobuf 3.6.1"
+echo "+ Python packages: scapy, ipaddr, psutil, crcmod, pypcap"
+echo ""
+echo "Note that anything installed as 'the latest version' can change"
+echo "its precise contents from one run of this script to another."
+echo "That is an intentional property of this script -- to get the"
+echo "latest version of that software.  If you want particular"
+echo "versions that are not the latest, you can modify this script by"
+echo "adding 'git checkout <tag>' and/or 'git checkout <commit-sha>'"
+echo "command at the appropriate places."
+echo ""
+
+echo "------------------------------------------------------------"
+echo "Time and disk space used before installation begins:"
+set -x
+date
+df -h .
+df -BM .
+
+# Check to see which versions of Python-related programs this system
+# already has installed, before the script starts installing things.
+lsb_release -a
+python -V  || echo "No such command in PATH: python"
+python2 -V || echo "No such command in PATH: python2"
+python3 -V || echo "No such command in PATH: python3"
+pip -V  || echo "No such command in PATH: pip"
+pip2 -V || echo "No such command in PATH: pip2"
+pip3 -V || echo "No such command in PATH: pip3"
+
+sudo apt-get install -qq -y --no-install-recommends --fix-missing\
+  curl \
+  git \
+  wget \
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Add repository with P4 packages
+# https://build.opensuse.org/project/show/home:p4lang
+
+echo "deb http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
+wget -qO - "http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/Release.key" | sudo apt-key add -
+
+sudo apt-get update -qq
+
+sudo apt-get install -qq -y --no-install-recommends --fix-missing\
+  iproute2 \
+  net-tools \
+  python3 \
+  python3-pip \
+  tcpdump \
+  unzip \
+  vim \
+  xterm \
+  p4lang-p4c \
+  p4lang-bmv2 \
+  p4lang-pi
+
+pip -V  || echo "No such command in PATH: pip"
+pip2 -V || echo "No such command in PATH: pip2"
+pip3 -V || echo "No such command in PATH: pip3"
+# At multiple points I do a 'pip list' command.  This is not required
+# for a successful installation -- I do it mainly because I am curious
+# to see in the log output files from running this script what
+# packages and versions were installed at those times during script
+# execution.
+pip list  || echo "Some error occurred attempting to run command: pip"
+pip3 list || echo "Some error occurred attempting to run command: pip3"
+
+cd "${INSTALL_DIR}"
+find /usr/lib /usr/local $HOME/.local | sort > usr-local-1-before-protobuf.txt
+
+# Starting in 2019-Nov, Python3 version of Scapy is needed for `cd
+# p4c/build ; make check` to succeed.
+# Earlier versions of this script installed the Ubuntu package
+# python-ipaddr.  However, that no longer exists in Ubuntu 20.04.  PIP
+# for Python3 can install the ipaddr module, which is good enough to
+# enable two of p4c's many tests to pass, tests that failed if the
+# ipaddr Python3 module is not installed, in my testing on
+# 2020-Oct-17.  From the Python stack trace that appears when running
+# those failing tests, the code that requires this module is in
+# behavioral-model's runtime_CLI.py source file, in a function named
+# ipv6Addr_to_bytes.
+sudo pip3 install -U scapy ipaddr ptf psutil grpcio
+
+echo "------------------------------------------------------------"
+
+echo "Installing Mininet - not necessary to run P4 programs, but useful if"
+echo "you want to run tutorials from https://github.com/p4lang/tutorials"
+echo "repository."
+echo "start install mininet:"
+set -x
+date
+
+git clone git://github.com/mininet/mininet mininet
+cd mininet
+PATCH_DIR="${THIS_SCRIPT_DIR_ABSOLUTE}/patches"
+patch -p1 < "${PATCH_DIR}/mininet-dont-install-python2.patch" || echo "Errors while attempting to patch mininet, but continuing anyway ..."
+cd ..
+sudo ./mininet/util/install.sh -nw
+
+set +x
+echo "end install mininet:"
+set -x
+date
+
+cd "${INSTALL_DIR}"
+find /usr/lib /usr/local $HOME/.local | sort > usr-local-7-after-mininet-install.txt
+
+# TODO: Needed?
+#sudo pip3 install pypcap
+
+#set +x
+#echo "------------------------------------------------------------"
+#echo "Installing a few miscellaneous packages"
+#echo "start install miscellaneous packages:"
+#set -x
+#date
+
+# Things needed for `cd tutorials/exercises/basic ; make run` to work:
+# TODO: Needed?
+#sudo apt-get --yes install libgflags-dev net-tools
+#sudo pip3 install psutil crcmod
+#pip3 list
+
+#set +x
+#echo "end install miscellaneous packages:"
+#set -x
+#date
+
+#cd "${INSTALL_DIR}"
+#find /usr/lib /usr/local $HOME/.local | sort > usr-local-9-after-miscellaneous-install.txt
+
+#pip list  || echo "Some error occurred attempting to run command: pip"
+#pip3 list
+
+#set +e
+
+set +x
+echo "------------------------------------------------------------"
+echo "Time and disk space used when installation was complete:"
+set -x
+date
+df -h .
+df -BM .
+
+cd "${INSTALL_DIR}"
+DETS="install-details"
+mkdir -p "${DETS}"
+mv usr-local-*.txt "${DETS}"
+cd "${DETS}"
