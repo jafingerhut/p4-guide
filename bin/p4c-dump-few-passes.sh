c49db3b49e8ca579a4584fca24d40ae33d0fb6f5
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2022-11-16 19:36:58 -0500

    Minor fix to p4c-dump-few-passes.sh script

--- bin/p4c-dump-few-passes.sh
+++ bin/p4c-dump-few-passes.sh
@@ -38,7 +38,7 @@ then
     P4RT_FILE1="${P4_SRC_FNAME}.p4info.txt"
     P4RT_FILE2="${P4_SRC_FNAME}.p4info.json"
     P4RT_OPTS="--p4runtime-files ${P4RT_FILE1},${P4RT_FILE2}"
-    p4c --target bmv2 --arch v1model ${PASSES_OPTS} ${P4RT_OPTS} "${P4_SRC_FNAME}"
+    p4c-bm2-ss --target bmv2 --arch v1model ${PASSES_OPTS} ${P4RT_OPTS} "${P4_SRC_FNAME}"
 else
-    p4c --target bmv2 --arch v1model ${PASSES_OPTS} "${P4_SRC_FNAME}"
+    p4c-bm2-ss --target bmv2 --arch v1model ${PASSES_OPTS} "${P4_SRC_FNAME}"
 fi
