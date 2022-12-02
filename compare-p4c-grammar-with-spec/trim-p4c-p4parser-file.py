19a06ff1b7b97cab4f16c0463d79219eaf9cc4cd
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2022-12-02 15:33:23 -0500

    Enhance Python program to eliminate blank lines in middle of nonterminal defns

--- compare-p4c-grammar-with-spec/trim-p4c-p4parser-file.py
+++ compare-p4c-grammar-with-spec/trim-p4c-p4parser-file.py
@@ -46,5 +46,34 @@ content = re.sub('%empty', '/* empty */', content)
 content = re.sub('l_angle', '"<"', content)
 content = re.sub('r_angle', '">"', content)
 
+# Remove blank lines that occur in the middle of the definition of a
+# non-terminal.  Relies on the following assumptions, which are
+# currently true of the file p4parser.ypp:
 
+# + Non-terminals are first non-whitespace character on a line.
+# + The ';' ending the definition of a non-terminal is always on a
+#   line by itself.
+
+lines = content.split('\n')
+lines2 = []
+
+i = 0
+while i < len(lines):
+    lines2.append(lines[i])
+    i += 1
+    nonterminal = re.match(r"^[a-zA-Z]", lines[i])
+    if nonterminal:
+        while i < len(lines):
+            end_of_defn = re.match(r"^\s+;", lines[i])
+            if end_of_defn:
+                lines2.append(lines[i])
+                i += 1
+                break
+            else:
+                blank = re.match(r"^\s*$", lines[i])
+                if not blank:
+                    lines2.append(lines[i])
+                i += 1
+
+content = '\n'.join(lines2)
 print(content)
