6a0e32cddc6d0c696e373d44c226a9bfb46e1613
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-01-06 11:37:57 -0500

    Create Python package p4runtime_shell_utils for commonly useful code across many P4Runtime examples

--- demo1/ptf/demo1.py
+++ demo1/ptf/demo1.py
@@ -22,7 +22,7 @@ import ptf
 import ptf.testutils as tu
 from ptf.base_tests import BaseTest
 import p4runtime_sh.shell as sh
-import p4runtime_sh.p4runtime as p4rt
+import p4runtime_shell_utils as p4rtutil
 
 
 # Links to many Python methods useful when writing automated tests:
@@ -68,71 +68,6 @@ logger.addHandler(ch)
 #logging.warn("30 logging.warn message")
 #logging.error("40 logging.error message")
 
-# Strongly inspired from _AssertRaisesContext in Python's unittest module
-class _AssertP4RuntimeErrorContext(object):
-    """A context manager used to implement the assertP4RuntimeError method."""
-
-    def __init__(self, test_case, error_code=None, msg_regexp=None):
-        self.failureException = test_case.failureException
-        self.error_code = error_code
-        self.msg_regexp = msg_regexp
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, tb):
-        if exc_type is None:
-            try:
-                exc_name = self.expected.__name__
-            except AttributeError:
-                exc_name = str(self.expected)
-            raise self.failureException(
-                "{} not raised".format(exc_name))
-
-        if not issubclass(exc_type, p4rt.P4RuntimeWriteException):
-            # let unexpected exceptions pass through
-            return False
-        self.exception = exc_value  # store for later retrieval
-
-        if self.error_code is None:
-            return True
-
-        expected_code_name = code_pb2._CODE.values_by_number[
-            self.error_code].name
-        # guaranteed to have at least one element
-        _, p4_error = exc_value.errors[0]
-        code_name = code_pb2._CODE.values_by_number[
-            p4_error.canonical_code].name
-        if p4_error.canonical_code != self.error_code:
-            # not the expected error code
-            raise self.failureException(
-                "Invalid P4Runtime error code: expected {} but got {}".format(
-                    expected_code_name, code_name))
-
-        if self.msg_regexp is None:
-            return True
-
-        if not self.msg_regexp.search(p4_error.message):
-            raise self.failureException(
-                "Invalid P4Runtime error msg: '{}' does not match '{}'".format(
-                self.msg_regexp.pattern, p4_error.message))
-        return True
-
-def assertP4RuntimeError(self, code=None, msg_regexp=None):
-    if msg_regexp is not None:
-        msg_regexp = re.compile(msg_regexp)
-    context = _AssertP4RuntimeErrorContext(self, code, msg_regexp)
-    return context
-
-def dump_table(table_name_str):
-    tes = []
-    def do_save_te(te):
-        tes.append(te)
-    sh.TableEntry(table_name_str).read(lambda te: do_save_te(te))
-    for te in tes:
-        logging.info(str(te))
-    logging.info("Table %s contains %d entries" % (table_name_str, len(tes)))
-
 class Demo1Test(BaseTest):
     def setUp(self):
         # Setting up PTF dataplane
@@ -149,13 +84,18 @@ class Demo1Test(BaseTest):
                  grpc_addr=grpc_addr,
                  election_id=(0, 1), # (high_32bits, lo_32bits)
                  config=sh.FwdPipeConfig(p4info_txt_fname, p4prog_binary_fname))
-        dump_table("ipv4_da_lpm")
-        dump_table("mac_da")
-        dump_table("send_frame")
+        p4rtutil.dump_table("ipv4_da_lpm")
+        p4rtutil.dump_table("mac_da")
+        p4rtutil.dump_table("send_frame")
 
     def tearDown(self):
         logging.info("Demo1Test.tearDown()")
         sh.teardown()
+
+#############################################################
+# Define a few small helper functions that make adding entries to
+# particular tables in demo2.p4 slightly shorter to write.
+#############################################################
 
 def add_ipv4_da_lpm_entry_action_set_l2ptr(ipv4_addr_str, prefix_len_int,
                                            l2ptr_int):
@@ -293,5 +233,5 @@ class DupEntryTest(Demo1Test):
             add_ipv4_da_lpm_entry_action_set_l2ptr(ip_dst_addr, 32, l2ptr)
 
         add_entry_once()
-        with assertP4RuntimeError(self):
+        with p4rtutil.assertP4RuntimeError(self):
             add_entry_once()
