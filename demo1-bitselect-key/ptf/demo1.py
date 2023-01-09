3c03ee902c0180feabe65d5f0c90188e118b2808
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-01-09 09:12:00 -0500

    Use p4runtime-shell for the PTF test in demo1-bitselect-key

--- demo1-bitselect-key/ptf/demo1.py
+++ demo1-bitselect-key/ptf/demo1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2021 Intel Corporation
+# Copyright 2023 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,13 +17,12 @@
 # Andy Fingerhut, andy.fingerhut@gmail.com
 
 import logging
-import os
+
 import ptf
 import ptf.testutils as tu
-
-from google.rpc import code_pb2
-
-import base_test as bt
+from ptf.base_tests import BaseTest
+import p4runtime_sh.shell as sh
+import p4runtime_shell_utils as p4rtutil
 
 
 # Links to many Python methods useful when writing automated tests:
@@ -35,11 +34,6 @@ import base_test as bt
 # documentation for these methods, here:
 
 # https://github.com/p4lang/ptf/blob/master/src/ptf/testutils.py
-
-# The package `base_test` is included in this repository, and can also
-# be seen at this link:
-
-# https://github.com/jafingerhut/p4-guide/blob/master/testlib/base_test.py
 
 
 ######################################################################
@@ -57,7 +51,7 @@ import base_test as bt
 
 logger = logging.getLogger(None)
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
@@ -74,51 +68,64 @@ logger.addHandler(ch)
 #logging.warn("30 logging.warn message")
 #logging.error("40 logging.error message")
 
-
-class Demo1Test(bt.P4RuntimeTest):
+class Demo1Test(BaseTest):
     def setUp(self):
-        bt.P4RuntimeTest.setUp(self)
-        # This setUp method will be executed once for each test case.
-        # It may be a little bit wasteful in time to load the compiled
-        # P4 program for each test, but for only a few tests it is
-        # still quick.  Suggestions welcome on good ways to load the
-        # compiled P4 program only once, yet still allow someone to
-        # select a subset of the test cases to be run from the `ptf`
-        # command line.
-        success = bt.P4RuntimeTest.updateConfig(self)
-        assert success
+        # Setting up PTF dataplane
+        self.dataplane = ptf.dataplane_instance
+        self.dataplane.flush()
 
-    #############################################################
-    # Define a few small helper functions that help construct
-    # parameters for the table_add() method.
-    #############################################################
+        logging.info("Demo1Test.setUp()")
+        grpc_addr = tu.test_param_get("grpcaddr")
+        if grpc_addr is None:
+            grpc_addr = 'localhost:9559'
+        p4info_txt_fname = tu.test_param_get("p4info")
+        p4prog_binary_fname = tu.test_param_get("config")
+        sh.setup(device_id=0,
+                 grpc_addr=grpc_addr,
+                 election_id=(0, 1), # (high_32bits, lo_32bits)
+                 config=sh.FwdPipeConfig(p4info_txt_fname, p4prog_binary_fname))
+        p4rtutil.dump_table("ipv4_da_lpm")
+        p4rtutil.dump_table("mac_da")
+        p4rtutil.dump_table("send_frame")
 
-    def key_ipv4_da_lpm(self, ipv4_addr_int, prefix_len):
-        return ('ipv4_da_lpm',
-                [self.Lpm('hdr.ipv4.dstAddr[23:8]',
-                          ipv4_addr_int, prefix_len)])
+    def tearDown(self):
+        logging.info("Demo1Test.tearDown()")
+        sh.teardown()
 
-    def act_set_l2ptr(self, l2ptr_int):
-        return ('set_l2ptr', [('l2ptr', l2ptr_int)])
+#############################################################
+# Define a few small helper functions that make adding entries to
+# particular tables in demo2.p4 slightly shorter to write.
+#############################################################
 
-    def key_mac_da(self, l2ptr_int):
-        return ('mac_da', [self.Exact('meta.fwd_metadata.l2ptr', l2ptr_int)])
+def add_ipv4_da_lpm_entry_action_set_l2ptr(ipv4_addr_int, prefix_len_int,
+                                           l2ptr_int):
+    te = sh.TableEntry('ipv4_da_lpm')(action='set_l2ptr')
+    # Note: p4runtime-shell raises an exception if you attempt to
+    # explicitly assign to te.match['dstAddr'] a prefix with length 0.
+    # Just skip assigning to te.match['dstAddr'] completely, and then
+    # inserting the entry will give a wildcard match for that field,
+    # as defined in the P4Runtime API spec.
+    if prefix_len_int != 0:
+        te.match['dstAddr[23:8]'] = '%d/%d' % (ipv4_addr_int, prefix_len_int)
+    te.action['l2ptr'] = '%d' % (l2ptr_int)
+    te.insert()
 
-    def act_set_bd_dmac_intf(self, bd_int, dmac_string, intf_int):
-        return ('set_bd_dmac_intf',
-                [('bd', bd_int),
-                 ('dmac', bt.mac_to_int(dmac_string)),
-                 ('intf', intf_int)])
+def add_mac_da_entry_action_set_bd_dmac_intf(l2ptr_int, bd_int, dmac_str,
+                                             intf_int):
+    te = sh.TableEntry('mac_da')(action='set_bd_dmac_intf')
+    te.match['l2ptr'] = '%d' % (l2ptr_int)
+    te.action['bd'] = '%d' % (bd_int)
+    te.action['dmac'] = dmac_str
+    te.action['intf'] = '%d' % (intf_int)
+    te.insert()
 
-    def key_send_frame(self, bd_int):
-        return ('send_frame', [self.Exact('meta.fwd_metadata.out_bd', bd_int)])
-
-    def act_rewrite_mac(self, smac_string):
-        return ('rewrite_mac', [('smac', bt.mac_to_int(smac_string))])
-
+def add_send_frame_entry_action_rewrite_mac(out_bd_int, smac_str):
+    te = sh.TableEntry('send_frame')(action='rewrite_mac')
+    te.match['out_bd'] = '%d' % (out_bd_int)
+    te.action['smac'] = smac_str
+    te.insert()
 
 class FwdTest(Demo1Test):
-    @bt.autocleanup
     def runTest(self):
         in_dmac = 'ee:30:ca:9d:1e:00'
         in_smac = 'ee:cd:00:7e:70:00'
@@ -138,15 +145,13 @@ class FwdTest(Demo1Test):
                                    ip_dst=ip_dst_addr, ip_ttl=64)
         tu.send_packet(self, ig_port, pkt)
         tu.verify_no_other_packets(self)
-        
+
         # Add a set of table entries that the packet should match, and
         # be forwarded out with the desired dest and source MAC
         # addresses.
-        self.table_add(self.key_ipv4_da_lpm(ip_dst_addr_middle, 8),
-                       self.act_set_l2ptr(l2ptr))
-        self.table_add(self.key_mac_da(l2ptr),
-                       self.act_set_bd_dmac_intf(bd, out_dmac, eg_port))
-        self.table_add(self.key_send_frame(bd), self.act_rewrite_mac(out_smac))
+        add_ipv4_da_lpm_entry_action_set_l2ptr(ip_dst_addr_middle, 8, l2ptr)
+        add_mac_da_entry_action_set_bd_dmac_intf(l2ptr, bd, out_dmac, eg_port)
+        add_send_frame_entry_action_rewrite_mac(bd, out_smac)
 
         # Check that the entry is hit, expected source and dest MAC
         # have been written into output packet, TTL has been
@@ -158,7 +163,6 @@ class FwdTest(Demo1Test):
 
 
 class PrefixLen0Test(Demo1Test):
-    @bt.autocleanup
     def runTest(self):
         in_dmac = 'ee:30:ca:9d:1e:00'
         in_smac = 'ee:cd:00:7e:70:00'
@@ -197,14 +201,12 @@ class PrefixLen0Test(Demo1Test):
                         'out_smac': '00:11:22:33:44:57'})
 
         for e in entries:
-            self.table_add(self.key_ipv4_da_lpm(e['ip_dst_addr'],
-                                                e['prefix_len']),
-                           self.act_set_l2ptr(e['l2ptr']))
-            self.table_add(self.key_mac_da(e['l2ptr']),
-                           self.act_set_bd_dmac_intf(e['bd'], e['out_dmac'],
-                                                     e['eg_port']))
-            self.table_add(self.key_send_frame(e['bd']),
-                           self.act_rewrite_mac(e['out_smac']))
+            add_ipv4_da_lpm_entry_action_set_l2ptr(e['ip_dst_addr'],
+                                                   e['prefix_len'], e['l2ptr'])
+            add_mac_da_entry_action_set_bd_dmac_intf(e['l2ptr'], e['bd'],
+                                                     e['out_dmac'],
+                                                     e['eg_port'])
+            add_send_frame_entry_action_rewrite_mac(e['bd'], e['out_smac'])
 
         ttl_in = 100
         for e in entries:
@@ -224,15 +226,13 @@ class PrefixLen0Test(Demo1Test):
 
 
 class DupEntryTest(Demo1Test):
-    @bt.autocleanup
     def runTest(self):
         ip_dst_addr = 0x0000 # '10.0.0.1'
         l2ptr = 58
 
         def add_entry_once():
-            self.table_add(self.key_ipv4_da_lpm(ip_dst_addr, 16),
-                           self.act_set_l2ptr(l2ptr))
+            add_ipv4_da_lpm_entry_action_set_l2ptr(ip_dst_addr, 16, l2ptr)
 
         add_entry_once()
-        with self.assertP4RuntimeError():
+        with p4rtutil.assertP4RuntimeError(self):
             add_entry_once()
