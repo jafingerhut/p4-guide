046ad85230940ec48d85d3ea1b9b8e18a3ca0a59
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-03-13 01:37:28 -0400

    Corrections to script for installing docker

--- ipdk/23.01/install-docker-ubuntu-20.04.sh
+++ ipdk/23.01/install-docker-ubuntu-20.04.sh
@@ -14,22 +14,16 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-```
 
 # The IPDK install steps require that you follow these "Linux
 # post-install steps" that enable a non-root user to run docker commands
 # without prefixing them with 'sudo', found here:
 # https://docs.docker.com/engine/install/linux-postinstall/
 
-```bash
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
 
 echo ""
-echo ""
-echo "This terminal window is now enabled to run docker commands without"
-echo "prefixing them with 'sudo'."
 echo ""
 echo "If you want all terminal windows to be able to run docker commands"
 echo "without using 'sudo', reboot the system."
