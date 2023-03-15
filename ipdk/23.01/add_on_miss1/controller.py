9204946a383f1c741969ee5436324e0663c340df
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-03-15 01:04:22 -0400

    Add initialization of entries in table set_ct_options for add_on_miss1.p4

--- ipdk/23.01/add_on_miss1/controller.py
+++ ipdk/23.01/add_on_miss1/controller.py
@@ -25,6 +25,22 @@ sh.setup(device_id=my_dev1_id,
          election_id=(0, 1),
          ssl_options=ssl_opts)
 
+def add_set_ct_options_entry_action_tcp_syn_packet(flags_value_int,
+                                                   flags_mask_int,
+                                                   priority_int):
+    te = sh.TableEntry('set_ct_options')(action='tcp_syn_packet')
+    te.match['flags'] = '%d&&&%d' % (flags_value_int, flags_mask_int)
+    te.priority = priority_int
+    te.insert()
+
+def add_set_ct_options_entry_action_tcp_fin_or_rst_packet(flags_value_int,
+                                                          flags_mask_int,
+                                                          priority_int):
+    te = sh.TableEntry('set_ct_options')(action='tcp_fin_or_rst_packet')
+    te.match['flags'] = '%d&&&%d' % (flags_value_int, flags_mask_int)
+    te.priority = priority_int
+    te.insert()
+
 def add_ipv4_host_entry_action_send(ipv4_addr_str, port_int):
     te = sh.TableEntry('ipv4_host')(action='send')
     te.match['dst_addr'] = '%s' % (ipv4_addr_str)
@@ -33,5 +49,16 @@ def add_ipv4_host_entry_action_send(ipv4_addr_str, port_int):
 
 add_ipv4_host_entry_action_send('1.1.1.1', 0)
 add_ipv4_host_entry_action_send('2.2.2.2', 1)
+
+TCP_URG_MASK = 0x20;
+TCP_ACK_MASK = 0x10;
+TCP_PSH_MASK = 0x08;
+TCP_RST_MASK = 0x04;
+TCP_SYN_MASK = 0x02;
+TCP_FIN_MASK = 0x01;
+
+add_set_ct_options_entry_action_tcp_syn_packet(TCP_SYN_MASK, TCP_SYN_MASK, 30)
+add_set_ct_options_entry_action_tcp_fin_or_rst_packet(TCP_FIN_MASK, TCP_FIN_MASK, 20)
+add_set_ct_options_entry_action_tcp_fin_or_rst_packet(TCP_RST_MASK, TCP_RST_MASK, 10)
 
 sh.teardown()
