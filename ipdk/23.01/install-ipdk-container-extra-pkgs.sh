e492fa327882ba6c496b7c995e6486876069725c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-07-16 21:31:10 -0400

    Fixes to the changes started in previous commit

--- ipdk/23.01/install-ipdk-container-extra-pkgs.sh
+++ ipdk/23.01/install-ipdk-container-extra-pkgs.sh
@@ -7,6 +7,20 @@
 # commands will fail.
 chmod 777 /tmp
 
+linux_version_warning() {
+    1>&2 echo "Found ID ${ID} and VERSION_ID ${VERSION_ID} in /etc/os-release"
+    1>&2 echo "This script only supports these:"
+    1>&2 echo "    ID ubuntu fedora"
+}
+
+if [ ! -r /etc/os-release ]
+then
+    1>&2 echo "No file /etc/os-release.  Cannot determine what OS this is."
+    linux_version_warning
+    exit 1
+fi
+source /etc/os-release
+
 if [ "${ID}" = "ubuntu" ]
 then
     apt-get update
