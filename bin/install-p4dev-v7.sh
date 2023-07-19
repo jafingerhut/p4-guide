a1001c3873dc72e1d8e10ad03df8e9c8c805468e
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-07-18 23:07:56 -0400

    Eliminate no-longer-needed extra debug output in install script

--- bin/install-p4dev-v7.sh
+++ bin/install-p4dev-v7.sh
@@ -550,22 +550,12 @@ sudo make install
 find /usr/lib /usr/local $HOME/.local "${PYTHON_VENV}" | sort > $HOME/usr-local-2b-before-grpc-pip3.txt
 pip3 list | tee $HOME/pip3-list-2b-before-grpc-pip3.txt
 cd ../..
-
-if [ "${ID}" = "ubuntu" ]
-then
-    dpkg -l | sort > $HOME/installed-pkgs.txt
-elif [ "${ID}" = "fedora" ]
-then
-    dnf list --installed | sort > $HOME/installed-pkgs.txt
-fi
-
 # Before some time in 2023-July, the `pip3 install -rrequirements.txt`
 # command below installed the Cython package version 0.29.35.  After
 # that time, it started installing Cython package version 3.0.0, which
 # gives errors on the `pip3 install .` command afterwards.  Fix this
 # by forcing installation of a known working version of Cython.
 ${PIP_SUDO} pip3 install Cython==0.29.35
-
 ${PIP_SUDO} pip3 install -rrequirements.txt
 GRPC_PYTHON_BUILD_WITH_CYTHON=1 ${PIP_SUDO} pip3 install .
 sudo ldconfig
