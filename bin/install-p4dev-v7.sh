cfd5f5fe62f441e7a1a2c195430486a0878612b2
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-02-07 03:49:46 +0000

    Add more frequent debug dumps of z3 installation, to see when it goes off

--- bin/install-p4dev-v7.sh
+++ bin/install-p4dev-v7.sh
@@ -826,6 +826,7 @@ set -x
 date
 
 cd "${INSTALL_DIR}"
+debug_dump_installed_z3_files snap3
 debug_dump_many_install_files ${INSTALL_DIR}/usr-local-3-after-grpc.txt
 
 set +x
@@ -902,6 +903,7 @@ set -x
 date
 
 cd "${INSTALL_DIR}"
+debug_dump_installed_z3_files snap4
 debug_dump_many_install_files ${INSTALL_DIR}/usr-local-4-after-PI.txt
 
 set +x
@@ -980,6 +982,7 @@ set -x
 date
 
 cd "${INSTALL_DIR}"
+debug_dump_installed_z3_files snap5
 debug_dump_many_install_files ${INSTALL_DIR}/usr-local-5-after-behavioral-model.txt
 
 set +x
@@ -1010,7 +1013,7 @@ then
          boost-devel boost-iostreams boost-graph \
          llvm llvm-devel pkgconf python3-pip tcpdump clang
 fi
-debug_dump_installed_z3_files snap3
+debug_dump_installed_z3_files snap6
 # Starting in 2019-Nov, Python3 version of Scapy is needed for `cd
 # p4c/build ; make check` to succeed.
 # ply package is needed for ebpf and ubpf backend tests to pass
@@ -1044,10 +1047,10 @@ else
     cd build
     # Configure for a debug build and build p4testgen
     cmake .. -DCMAKE_BUILD_TYPE=DEBUG ${P4C_CMAKE_OPTS}
-    debug_dump_installed_z3_files snap4
+    debug_dump_installed_z3_files snap7
     MAX_PARALLEL_JOBS=`max_parallel_jobs 2048`
     make -j${MAX_PARALLEL_JOBS}
-    debug_dump_installed_z3_files snap5
+    debug_dump_installed_z3_files snap8
     sudo make install/strip
     sudo ldconfig
     if [ ${CLEAN_UP_AS_WE_GO} -eq 1 -a ${KEEP_P4C_BUILD_FOR_TESTING} -eq 0 ]
