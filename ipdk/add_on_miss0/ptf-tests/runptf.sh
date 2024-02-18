fd1531cedfacad461db2a4bb1850c56b5064d846
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-02-18 00:14:06 -0500

    Correct runptf.sh scripts for IPDK, and add some debugging notes.

--- ipdk/add_on_miss0/ptf-tests/runptf.sh
+++ ipdk/add_on_miss0/ptf-tests/runptf.sh
@@ -15,10 +15,10 @@ ptf \
     -i 1@TAP1 \
     -i 2@TAP2 \
     -i 3@TAP3 \
-    -i 17@TAP4 \
-    -i 18@TAP5 \
-    -i 19@TAP6 \
-    -i 20@TAP7 \
+    -i 4@TAP4 \
+    -i 5@TAP5 \
+    -i 6@TAP6 \
+    -i 7@TAP7 \
     --test-params="grpcaddr='localhost:9559'" \
     --test-dir .
 set +x
--- ipdk/sample/runptf.sh
+++ ipdk/sample/runptf.sh
@@ -8,6 +8,13 @@ else
     P="${T}:${PYTHONPATH}"
 fi
 
+# Note: The bash script `setup_tapports_in_default_ns.sh` creates N
+# ports such that port number j in the P4 program corresponds to Linux
+# interface TAP<j>, e.g. port 3 corresponds to Linux interface TAP3.
+# The ptf command line options use this mapping.  Changing them is
+# likely to confuse you when writing PTF tests that send and receive
+# packets.
+
 set -x
 ptf \
     --pypath "$P" \
@@ -15,10 +22,10 @@ ptf \
     -i 1@TAP1 \
     -i 2@TAP2 \
     -i 3@TAP3 \
-    -i 17@TAP4 \
-    -i 18@TAP5 \
-    -i 19@TAP6 \
-    -i 20@TAP7 \
+    -i 4@TAP4 \
+    -i 5@TAP5 \
+    -i 6@TAP6 \
+    -i 7@TAP7 \
     --test-params="grpcaddr='localhost:9559'" \
     --test-dir .
 set +x
