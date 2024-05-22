b6b6646c42708c85482b0d55d50bfb186b64878c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-05-22 09:12:33 -0700

    Fix bug in creation of p4setup.* files

--- bin/install-p4dev-v8.sh
+++ bin/install-p4dev-v8.sh
@@ -1363,7 +1363,7 @@ echo "export P4GUIDE_SUDO_OPTS=\"PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python\"
 cp /dev/null p4setup.csh
 echo "source ${INSTALL_DIR}/p4dev-python-venv/bin/activate.csh" >> p4setup.csh
 echo "set path = ( ${P4GUIDE_BIN} ${INSTALL_DIR}/behavioral-model/tools /usr/local/bin \$path )" >> p4setup.csh
-echo "setenv P4GUIDE_SUDO_OPTS \"PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python\"" >> p4setup.bash
+echo "setenv P4GUIDE_SUDO_OPTS \"PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python\"" >> p4setup.csh
 
 echo ""
 echo "Created files: p4setup.bash p4setup.csh"
