b04c7d78a0740f2150ce930132ec7cf47129406c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-06-25 10:56:37 -0400

    Add option to read packets from pcap file

--- golang/trypcap/trypcap.go
+++ golang/trypcap/trypcap.go
@@ -3,7 +3,7 @@
 
 // trypcap exercises a few features of the pcap library from Go
 
-package trypcap
+package main
 
 import (
 	"flag"
@@ -13,37 +13,62 @@ import (
 	//"time"
 
 	"github.com/google/gopacket"
+	//"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
 // Things in pcap package:
 // func FindAllDevs() (ifs []Interface, err error)
 
-var iface = flag.String("iface", "en0", "Select interface where to capture")
+var iface = flag.String("iface", "", "Select interface where to capture")
+var fname = flag.String("file", "", "pcap file to read packets from")
+
+func printUsage(progName string) {
+	fmt.Fprintf(os.Stderr, "usage: %s\n", progName)
+	fmt.Fprintln(os.Stderr, "            [ --iface <interface_name> ]")
+	fmt.Fprintln(os.Stderr, "            [ --file <pcap_filename> ]")
+	fmt.Fprintln(os.Stderr)
+}
 
 func main() {
 	flag.Parse()
 
-	intfs, err := pcap.FindAllDevs()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	fmt.Printf("Found %d interfaces:\n\n", len(intfs))
-	for idx, intf := range intfs {
-		fmt.Printf("%2d 0x%04x %s %s %d addresses: %v\n", idx, intf.Flags, intf.Name, intf.Description, len(intf.Addresses), intf.Addresses)
+	if *iface == "" && *fname == "" {
+		printUsage(os.Args[0])
+		os.Exit(1)
 	}
 
-	// Opening Device
-	snaplen := 10000
-	promisc := true
-	//timeout := 30 * time.Second
-	timeout := pcap.BlockForever
-	handle, err := pcap.OpenLive(*iface, int32(snaplen), promisc, timeout)
-	if err != nil {
-		log.Fatal(err)
+	var handle *pcap.Handle
+	if *iface != "" {
+		intfs, err := pcap.FindAllDevs()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		fmt.Printf("Found %d interfaces:\n\n", len(intfs))
+		for idx, intf := range intfs {
+			fmt.Printf("%2d 0x%04x %s %s %d addresses: %v\n", idx, intf.Flags, intf.Name, intf.Description, len(intf.Addresses), intf.Addresses)
+		}
+
+		// Opening Device
+		snaplen := 10000
+		promisc := true
+		//timeout := 30 * time.Second
+		timeout := pcap.BlockForever
+		handle, err = pcap.OpenLive(*iface, int32(snaplen), promisc, timeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Opened interface %s promisc=%v\n", *iface, promisc)
 	}
-	fmt.Printf("Opened interface %s promisc=%v\n", *iface, promisc)
+	if *fname != "" {
+		var err error
+		handle, err = pcap.OpenOffline(*fname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Opened file %s\n", *fname)
+	}
 	fmt.Printf("   handle=%v\n", handle)
 	defer handle.Close()
 
@@ -53,6 +78,8 @@ func main() {
 		n += 1
 		fmt.Printf("\n%d\n", n)
 		fmt.Println(packet)
-		//fmt.Printf("%v\n", packet)
+
+		//p := gopacket.NewPacket(packet, CiscoS1PuntHeaderType, gopacket.Lazy)
+		//fmt.Println(p)
 	}
 }
