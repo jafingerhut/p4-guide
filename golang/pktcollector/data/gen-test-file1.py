805b7b480b4ab0d2574c20f104a3241ba14f277d
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-07-10 15:20:05 -0400

    Add short pcap test file I have confirmed that the time stamps recorded in the file have the same separation in time as the time.sleep() calls in the Python program might lead one to guess they have.

--- golang/pktcollector/data/gen-test-file1.py
+++ golang/pktcollector/data/gen-test-file1.py
@@ -6,7 +6,7 @@ from scapy.all import *
 pkts = []
 for i in range(4):
     smac = ('00:11:22:33:44:%x%x' % (i, i))
-    pkts.append(Ether(src=smac, dmac='00:00:00:00:00:05') / IP(dst='10.1.0.1', src='10.2.2.2'))
+    pkts.append(Ether(src=smac, dst='00:00:00:00:00:05') / IP(dst='10.1.0.1', src='10.2.2.2'))
     time.sleep(i)
 
 wrpcap('test-pkts1.pcap', pkts)
