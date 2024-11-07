1b8b56410fdad01ffdd701f9b8d8fb9c635a514d
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-11-07 19:43:57 +0000

    Update p4c-delete-duplicate-passes.sh I think some time in the past year or two, the names of the p4c output files produces by the `--top4` option changed in a way that broke this script's assumptions.

--- bin/p4c-delete-duplicate-passes.sh
+++ bin/p4c-delete-duplicate-passes.sh
@@ -19,76 +19,25 @@ cd "${DUMP_DIR}"
 
 #set -vx
 
-frontendpass=0
-lastkeptfile="not found"
-for n in ${BNAME}-FrontEnd_${frontendpass}_*.p4*
+lsatkeptfile=""
+for curname in ${BNAME}-*.p4
 do
-    lastkeptfile="${n}"
-done
-if [ "${lastkeptfile}" == "not found" ]
-then
-    1>&2 echo "No such file: ${lastkeptfile}"
-    exit 1
-fi
-
-for frontendpass in `seq 1 100`
-do
-    for n in ${BNAME}-FrontEnd_${frontendpass}_*.p4*
-    do
-	curname="${n}"
-    done
-    if [ "${curname}" == "not found" ]
+    if [ "${lastkeptfile}" == "" ]
     then
-        1>&2 echo "No such file: ${curname}"
-        exit 1
-    fi
-    if [ ! -e "${curname}" ]
-    then
-	echo "# No file with name ${curname}.  Probably no such compiler pass."
-	break
-    fi
-    diff "${lastkeptfile}" "${curname}" > /dev/null
-    status=$?
-    if [ ${status} == 0 ]
-    then
-	# Identical files, so remove the next one
-	echo "# Deleting ${curname} as duplicate of ${lastkeptfile}"
-	/bin/rm -f "${curname}"
-    else
-	# Files differ, so remember the current one as the last kept
-	# one.
-	echo "ediff ${DUMP_DIR}/${lastkeptfile} ${DUMP_DIR}/${curname}"
 	lastkeptfile="${curname}"
-    fi
-done
-
-for midendpass in `seq 0 100`
-do
-    for n in ${BNAME}-*MidEnd_${midendpass}_*.p4*
-    do
-	curname="${n}"
-    done
-    if [ "${curname}" == "not found" ]
-    then
-        1>&2 echo "No such file: ${curname}"
-        exit 1
-    fi
-    if [ ! -e "${curname}" ]
-    then
-	echo "# No file with name ${curname}.  Probably no such compiler pass."
-	break
-    fi
-    diff "${lastkeptfile}" "${curname}" > /dev/null
-    status=$?
-    if [ ${status} == 0 ]
-    then
-	# Identical files, so remove the next one
-	echo "# Deleting ${curname} as duplicate of ${lastkeptfile}"
-	/bin/rm -f "${curname}"
     else
-	# Files differ, so remember the current one as the last kept
-	# one.
-	echo "ediff ${DUMP_DIR}/${lastkeptfile} ${DUMP_DIR}/${curname}"
-	lastkeptfile="${curname}"
+	diff "${lastkeptfile}" "${curname}" > /dev/null
+	status=$?
+	if [ ${status} == 0 ]
+	then
+	    # Identical files, so remove the next one
+	    echo "# Deleting ${curname} as duplicate of ${lastkeptfile}"
+	    /bin/rm -f "${curname}"
+	else
+	    # Files differ, so remember the current one as the last kept
+	    # one.
+	    echo "ediff ${DUMP_DIR}/${lastkeptfile} ${DUMP_DIR}/${curname}"
+	    lastkeptfile="${curname}"
+	fi
     fi
 done
