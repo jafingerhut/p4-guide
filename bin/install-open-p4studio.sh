d440d60dac3e501af7c832750bd3559335d08d1e
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-02-09 07:45:05 +0000

    Add SPDX license id comment lines to many files that were missing them

--- bin/install-open-p4studio.sh
+++ bin/install-open-p4studio.sh
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 # On x86_64 Ubuntu 20.04.6 system, with no build-essential package installed.
 # Note that having at least 4 GBytes of RAM per CPU core,
--- bin/install-p4dev-v4.sh
+++ bin/install-p4dev-v4.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Copyright 2020-present Intel Corporation
+# Copyright 2020 Intel Corporation
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 
 # This script differs from install-p4dev-v3.sh as follows:
