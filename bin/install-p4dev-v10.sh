4063570eeada0f7ee5b32a677fa51c9256332d55
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-09-24 18:26:39 -0400

    Add some more env variable assignments to p4setup.bash / .csh

--- bin/install-p4dev-v10.sh
+++ bin/install-p4dev-v10.sh
@@ -1112,12 +1112,14 @@ echo ""
 cd "${INSTALL_DIR}"
 cp /dev/null p4setup.bash
 echo "source ${INSTALL_DIR}/p4dev-python-venv/bin/activate" >> p4setup.bash
-echo "export PATH=\"${P4GUIDE_BIN}:${INSTALL_DIR}/behavioral-model/tools:/usr/local/bin:\$PATH\"" >> p4setup.bash
+echo "export UV_PROJECT_ENVIRONMENT=\${VIRTUAL_ENV}" >> p4setup.bash
+echo "export PATH=\"${HOME}/.local/bin:${P4GUIDE_BIN}:${INSTALL_DIR}/behavioral-model/tools:/usr/local/bin:\$PATH\"" >> p4setup.bash
 echo "export P4_EXTRA_SUDO_OPTS=\"PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python\"" >> p4setup.bash
 
 cp /dev/null p4setup.csh
 echo "source ${INSTALL_DIR}/p4dev-python-venv/bin/activate.csh" >> p4setup.csh
-echo "set path = ( ${P4GUIDE_BIN} ${INSTALL_DIR}/behavioral-model/tools /usr/local/bin \$path )" >> p4setup.csh
+echo "setenv UV_PROJECT_ENVIRONMENT \${VIRTUAL_ENV}" >> p4setup.csh
+echo "set path = ( ${HOME}/.local/bin ${P4GUIDE_BIN} ${INSTALL_DIR}/behavioral-model/tools /usr/local/bin \$path )" >> p4setup.csh
 echo "setenv P4_EXTRA_SUDO_OPTS \"PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python\"" >> p4setup.csh
 
 echo "If you use a Bash-like command shell, you may wish to add a line like"
